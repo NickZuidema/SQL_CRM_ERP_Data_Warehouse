32231ef7454383a7a4b40b2f0639be2490018452
Nick Zuidema <155800402+NickZuidema@users.noreply.github.com>
2025-11-19 13:13:30 -0800

    Finalized Advanced Data Analytics
    advanced_data_analysis.sql now contains Queries to show Change-Over-Time, Cumulative Analysis, Performance Analysis, Part-to-Whole Analysis, and Data Segmentation.

--- scripts/advanced_data_analysis.sql
+++ scripts/advanced_data_analysis.sql
@@ -76,3 +76,96 @@ SELECT
 	END [avg_change]
 FROM yearly_product_sales
 ORDER BY product_name, year
+
+--===================================
+--Part-to-Whole Analysis
+--===================================
+SELECT 
+	*
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA='gold'
+
+--Which categories contribute the most to overall sales?
+WITH category_sales AS(
+	SELECT
+		p.category [category],
+		SUM(s.sales_amount) [sales]
+	FROM gold.fact_sales s
+	INNER JOIN gold.dim_products p
+	ON s.product_key=p.product_key
+	GROUP BY p.category
+)
+
+SELECT
+	category,
+	sales,
+	SUM(sales) OVER() [total_sales],
+	CONCAT(ROUND((CAST(sales AS float)/SUM(sales) OVER()) * 100, 2), '%')[percentage]
+FROM category_sales
+ORDER BY sales DESC
+
+
+--===================================
+--Data Segmentation
+--===================================
+--Segment products into cost ranges and count how many products fall into each segment.
+WITH cost_segmentation AS (
+	SELECT
+		product_name,
+		cost,
+		CASE 
+			WHEN COST < 100 THEN 'Below 100'
+			WHEN COST BETWEEN 100 AND 500 THEN '100 - 500'
+			WHEN COST BETWEEN 500 AND 1000 THEN '500 - 1000'
+			ELSE 'Above 1000'
+		END [cost_range]
+	FROM gold.dim_products
+	--ORDER BY cost ASC
+)
+
+SELECT
+	cost_range,
+	COUNT(1) [products]
+FROM cost_segmentation
+GROUP BY cost_range
+ORDER BY 2 DESC
+
+/*Group customers into three segments based on their spending behavior:
+	-VIP: Customers with at least 12 months of history and spending more than $5,000.
+	-Regular: Customers with at least 12 months of history but spending $5,000 or less.
+	-New: Customers with a lifespan less than 12 months.
+And find the total number of customers by each group.*/
+WITH customer_segmentation AS(
+	SELECT 
+		s.customer_key,
+		MIN(s.order_date) [earliest_order],
+		MAX(s.order_date) [latest_order],
+		DATEDIFF(MONTH, MIN(s.order_date), MAX(s.order_date)) [months_of_history],
+		SUM(sales_amount) [total_sales],
+		CASE
+			WHEN 
+				DATEDIFF(MONTH, MIN(s.order_date), MAX(s.order_date)) >= 12 
+				AND SUM(sales_amount) > 5000
+					THEN 'VIP'
+			WHEN
+				DATEDIFF(MONTH, MIN(s.order_date), MAX(s.order_date)) >= 12 
+				AND SUM(sales_amount) <= 5000
+					THEN 'Regular'
+			ELSE
+					'New'
+		END [customer_class]
+	FROM gold.fact_sales s
+	INNER JOIN gold.dim_customers c
+		ON s.customer_key=c.customer_key
+	GROUP BY s.customer_key
+	--ORDER BY 6 DESC
+)
+
+SELECT
+	customer_class,
+	COUNT(1) [class_count]
+FROM customer_segmentation
+GROUP BY customer_class
+
+
+
